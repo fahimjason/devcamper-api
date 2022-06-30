2f606c443c26511f6a41dfbef6d95296e6c92881
JisanFahim <fahimjason@users.noreply.github.com>
2022-06-30 14:21:22 +0600

    added-slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootCampSchema = new mongoose.Schema({
     name: {
@@ -96,6 +97,12 @@ const BootCampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+});
+
+// Create bootcamp slug from the name
+BootCampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootCampSchema);
