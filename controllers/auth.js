ae1be2fd755984c4858da266b1cb7ae1d7727bea
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-10 21:54:37 +0600

    created-user-model

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const User = require('../models/User');
+
+// @desc      Register user
+// @route     POST /api/v1/auth/register
+// @access    Public
+exports.register = asyncHandler(async (req, res, next) => {
+    // const { name, email, password, role } = req.body;
+
+    // // Create user
+    // const user = await User.create({
+    //     name,
+    //     email,
+    //     password,
+    //     role
+    // });
+
+    // sendTokenResponse(user, 200, res);
+
+    res.status(201).json({ success: true })
+});
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
