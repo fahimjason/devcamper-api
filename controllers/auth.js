3506e374260aa45c7b226c2bc341c59ba2263be1
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-10 22:31:04 +0600

    done-password-encrypting

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,15 +6,15 @@ const User = require('../models/User');
 // @route     POST /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
-    // const { name, email, password, role } = req.body;
+    const { name, email, password, role } = req.body;
 
-    // // Create user
-    // const user = await User.create({
-    //     name,
-    //     email,
-    //     password,
-    //     role
-    // });
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
 
     // sendTokenResponse(user, 200, res);
 
