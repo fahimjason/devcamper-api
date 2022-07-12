e5b68547fcb78ce4c11c22511bd5eb5308a728a6
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-12 20:01:52 +0600

    created-user-login-route

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,3 +23,34 @@ exports.register = asyncHandler(async (req, res, next) => {
 
     res.status(201).json({ success: true, token })
 });
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate emil & password
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(201).json({ success: true, token });
+});
