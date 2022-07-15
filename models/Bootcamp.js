eb063a77f42777a9faca0854ea90fb3f66616d06
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-15 23:45:13 +0600

    done-bootcamp-user-relationship

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -97,6 +97,11 @@ const BootCampSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
     }
 },
     {
--- seeder.js
+++ seeder.js
@@ -9,7 +9,7 @@ dotenv.config({ path: './config/config.env' });
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
-// const User = require('./models/User');
+const User = require('./models/User');
 // const Review = require('./models/Review');
 
 // Connect to DB
@@ -27,9 +27,9 @@ const courses = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
-// const users = JSON.parse(
-//     fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
-// );
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
 
 // const reviews = JSON.parse(
 //     fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
@@ -40,6 +40,7 @@ const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (err) {
@@ -52,6 +53,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (err) {
