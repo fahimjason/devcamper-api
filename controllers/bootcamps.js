c90ff563ebcc373308a9f48f0f348e0ef73870cc
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-16 22:53:24 +0600

    implemented-bootcamp-ownership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -83,7 +83,7 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
     // Make sure user is bootcamp owner
     if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
-        return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401));
+        return next(new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`, 401));
     }
 
     bootcamp.remove();
