86e0caf03768533b8cb09e4bf6e4d7021aee6d43
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-18 18:33:13 +0600

    done-generate-token-forgot-password

--- controllers/auth.js
+++ controllers/auth.js
@@ -81,3 +81,24 @@ exports.getMe = asyncHandler(async (req, res, next) => {
         data: user
     });
 });
+
+// @desc      Forgot password
+// @route     POST /api/v1/auth/forgotpassword
+// @access    Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorResponse('There is no user with that email', 404));
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,8 @@ const express = require('express');
 const {
     register,
     login,
-    getMe
+    getMe,
+    forgotPassword
 } = require('../controllers/auth');
 
 const { protect } = require('../middleware/auth');
@@ -12,5 +13,6 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.post('/forgotpassword', forgotPassword);
 
 module.exports = router;
