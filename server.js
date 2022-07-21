23b3f127b8c0c67e5c4c0aabf08dc05f660fbe22
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-21 22:11:05 +0600

    fixed-admin-users-route-issue

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
 
 app.use(errorHandler);
 
