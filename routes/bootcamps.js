57a31838e8582f14e4d1ac4d61d50eeac366b8ea
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-25 23:40:24 +0600

    created-reviews-model-controller-and-route-files

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,16 +10,19 @@ const {
 } = require('../controllers/bootcamps');
 
 const Bootcamp = require('../models/Bootcamp');
-const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
-const { protect, authorize } = require('../middleware/auth');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorize } = require('../middleware/auth');
+
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
