e03d5ea9c8965466fe3f1d6654dac3609054a2af
JisanFahim <fahimjason@users.noreply.github.com>
2022-07-29 23:03:49 +0600

    added-helmet

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -41,6 +42,9 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
